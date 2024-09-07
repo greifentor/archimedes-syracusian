4031f28809b43d0427c1db1358e5f559b60414e5
ollie <lieshoff@gmx.de>
2024-09-07 19:47:43 +0200

    Cleaned up Maven configuration and added a first class for testing.

---
+++ importer/src/main/java/de/ollie/archimedes/syracusian/importer/DevStarter.java
@@ -0,0 +1,14 @@
+package de.ollie.archimedes.syracusian.importer;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Named;
+
+@Named
+public class DevStarter {
+
+	@PostConstruct
+	void postConstruct() {
+		System.out.println("\n\nHello!\n\n");
+	}
+
+}
